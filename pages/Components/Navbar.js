de1ded0ee433055e33b39b228a45ffdb79d2c312
Kmal4li <akmaltamvan135@gmail.com>
2023-11-02 17:15:05 +0700

    first commit

---
+++ pages/Components/Navbar.js
@@ -0,0 +1,22 @@
+import Link from "next/link"
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <link href="\">Home</link>
+                </li>
+                <li>
+                    <link href="\about">About</link>
+                </li>
+                <li>
+                    <link href="\profile">Profile</link>
+                </li>
+                <li>
+                    <link href="\contact">Contact</link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
